c4e284b6ed0cc12493502567d11170d0012bfd38
dna750d <alnjfy113@gmail.com>
2026-07-21 00:33:32 +0300

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyDqZqjsgoG7CdTQC6cgJYq2Bl58GBd0SnA",
-  authDomain: "codenames-ar.firebaseapp.com",
-  databaseURL: "https://codenames-ar-default-rtdb.firebaseio.com",
-  projectId: "codenames-ar",
-  appId: "1:496850043713:web:fe051576956f1de70517c0",
-};
-firebase.initializeApp(firebaseConfig);
-const db = firebase.database();
